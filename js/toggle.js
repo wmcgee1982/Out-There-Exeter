eaf266b8c0c86acda3f44cfb13f43a5c77542830
wmcgee1982 <wayne@atashvana.co.uk>
2024-04-03 20:26:43 +0100

    started making it repsonsive. Addad JS for take part section

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,14 @@
-function myFunction() {
+function toggle() {
     var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function toggle1() {
+    var x = document.getElementById("myLinks1");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
