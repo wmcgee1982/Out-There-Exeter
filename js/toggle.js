0449997cba8df2bdd0c6d83f2ff145a4397cab14
wmcgee1982 <wayne@atashvana.co.uk>
2024-04-22 16:48:59 +0100

    hiding elements on menu

--- js/toggle.js
+++ js/toggle.js
@@ -10,14 +10,11 @@ function toggle() {
   function toggle1() {
     var x = document.getElementById("myLinks-small");
     var y = document.getElementById("main-hide-small");
-    var z = document.getElementById("background-small");
     if (x.style.display === "block") {
       x.style.display = "none";
       y.style.display = "block";
-      z.style.display = "block";
     } else {
       x.style.display = "block";
-      y.style.display = "none";
       y.style.display = "none";
     }
   }
