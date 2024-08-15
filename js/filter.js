863876aebc6393f2d947220f3754123f898a1a46
wmcgee1982 <wayne@atashvana.co.uk>
2024-08-15 17:17:07 +0100

    updated things

--- js/filter.js
+++ js/filter.js
@@ -10,3 +10,15 @@ function filterEvents(category) {
         }
     });
 }
+// JavaScript function to filter events
+function filterEvents(category) {
+    var events= document.querySelectorAll('.event-cat-small');
+    
+    events.forEach(function(event) {
+        if (category === 'all' || event.getAttribute('data-category') === category) {
+            event.classList.remove('hidden');
+        } else {
+            event.classList.add('hidden');
+        }
+    });
+}
