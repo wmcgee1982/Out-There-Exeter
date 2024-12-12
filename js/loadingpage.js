cd19afd3861bfa8a2e05d89f373fb95a0d1ba517
wmcgee1982 <wayne@atashvana.co.uk>
2024-12-12 16:01:51 +0000

    changed image sizes

--- js/loadingpage.js
+++ js/loadingpage.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loadingElement = document.getElementById("loading-page-small");
+  const mainElement = document.getElementById("main");
+  const mainSmallElement = document.getElementById("main-small");
+
+  // Hide loading screen and reveal #main after 3 seconds
   setTimeout(() => {
       loadingElement.style.display = "none";
-  }, 3000); // Match this duration with your animation duration
+      mainElement.style.opacity = 1; // Ensure visibility
+      mainSmallElement.style.opacity = 1; // Ensure visibility
+  }, 4000); // Matches the animation duration of #loading-page-small
 });
