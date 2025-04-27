61a4f8f319d49e330dc98fdaa7e08bd52845a8bd
wmcgee1982 <wayne@atashvana.co.uk>
2025-04-27 20:54:11 +0100

    amended front page

--- js/arrowbounce.js
+++ js/arrowbounce.js
@@ -1,3 +1,25 @@
 function scrollWin() {
     window.scrollBy(0, window.innerHeight);
   }
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const images = Array.from(document.querySelectorAll('.square-wrapper img'));
+    const shuffled = images.sort(() => 0.5 - Math.random());
+    let imagesShown = 0;
+  
+    setTimeout(() => {
+      shuffled.forEach((img, index) => {
+        setTimeout(() => {
+          img.classList.add('show');
+          imagesShown++;
+  
+          if (imagesShown === shuffled.length) {
+            shuffled.forEach(image => image.classList.add('loaded'));
+          }
+        }, index * 30);
+      });
+    }, 4000);
+  });
+  
